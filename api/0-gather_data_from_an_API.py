390266de745c47ceb5505e95e5116372e1b2b032
orugamba-ux <o.rugamba@alustudent.com>
2026-03-26 17:25:18 +0200

    Update 0-gather_data_from_an_API.py with new changes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,9 +7,11 @@ import sys
 import requests
 if __name__ == "__main__":
     user_id = int(sys.argv[1])
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    user_url = ("https://jsonplaceholder.typicode.com/users/{}"
+                .format(user_id))
     user = requests.get(user_url).json()
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
+    todos_url = ("https://jsonplaceholder.typicode.com/todos?userId={}"
+                 .format(user_id))
     todos = requests.get(todos_url).json()
     total_tasks = len(todos)
     done_tasks = 0
