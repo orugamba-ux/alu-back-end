2060fe1732482f264ea1819b5d399b6f0fdc5bfb
orugamba-ux <o.rugamba@alustudent.com>
2026-03-26 16:45:32 +0200

    Update API scripts and export functions

--- 1-export_to_CSV.py
+++ 1-export_to_CSV.py
@@ -2,21 +2,16 @@
 import requests
 import sys
 import csv
-
 if __name__ == "__main__":
     user_id = sys.argv[1]
-
     user = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     ).json()
-
     todos = requests.get(
         "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
     ).json()
-
     with open("{}.csv".format(user_id), "w", newline="") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-
         for task in todos:
             writer.writerow([
                 user_id,
--- 3-dictionary_of_list_of_dictionaries.py
+++ 3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 import requests
 import json
-
 if __name__ == "__main__":
-
     users = requests.get(
         "https://jsonplaceholder.typicode.com/users"
     ).json()
-
     todos = requests.get(
         "https://jsonplaceholder.typicode.com/todos"
     ).json()
-
     data = {}
-
     for user in users:
         user_id = str(user.get("id"))
         username = user.get("username")
-
         data[user_id] = []
-
         for task in todos:
             if task.get("userId") == user.get("id"):
                 data[user_id].append({
@@ -27,6 +20,5 @@ if __name__ == "__main__":
                     "task": task.get("title"),
                     "completed": task.get("completed")
                 })
-
     with open("todo_all_employees.json", "w") as f:
         json.dump(data, f)
